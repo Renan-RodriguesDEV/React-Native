d78d58da54d723e42be7cd3c081ce7e6411b454f
Renan RodriguesDev <renanrodrigues7110@gmail.com>
2025-03-19 11:58:20 -0300

    fix: alterando codigo legivel

--- fletlist/app/(tabs)/index.tsx
+++ fletlist/app/(tabs)/index.tsx
@@ -10,62 +10,52 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const App = () => {
+  // declaração das variaveis a serem utilizadas
   const [texto, setTexto] = useState("");
-  const [numero, setNumero] = useState(0);
-  const [storageData, setStorageData] = useState<
-    Array<{ key: string; value: string | null }>
-  >([]);
+  const [texts, setTexts] = useState([]);
 
-  async function fetchStorageData() {
-    try {
-      const keys = await AsyncStorage.getAllKeys();
-      const results = await AsyncStorage.multiGet(keys);
-      const data = results.map(([key, value]) => ({ key, value }));
-      setStorageData(data);
-    } catch (error) {
-      console.error("Erro ao iterar AsyncStorage:", error);
-    }
-  }
-
-  async function salvarChaveValor() {
-    try {
-      setNumero(numero + 1);
-      await AsyncStorage.setItem(numero.toString(), texto);
-      fetchStorageData();
-    } catch (error) {
-      console.error("Erro ao salvar no AsyncStorage:", error);
-    }
-  }
-
-  async function apagarChaves() {
-    try {
-      setNumero(0);
-      await AsyncStorage.clear();
-      fetchStorageData();
-    } catch (error) {
-      console.error("Erro ao limpar AsyncStorage:", error);
-    }
-  }
-
+  // ao montar o componente, carrega os valores salvos no storage
   useEffect(() => {
-    fetchStorageData();
+    loadTexts();
   }, []);
 
+  async function loadTexts() {
+    // pega os valores salvos no storage na chave texts
+    const textsSaved = await AsyncStorage.getItem("texts");
+    // se existir, converte o valor de string para um JSON (q representa um array) em um array real usando JSON.parse e atualiza à variavel texts com setTexts
+    if (textsSaved) setTexts(JSON.parse(textsSaved));
+  }
+
+  async function saveText() {
+    // elimina os espaços e verifica se a palavra é nada
+    if (texto.trim() == "") return;
+    // cria um novo array a partir do já existente adicionando o valor atual de texto
+    const newTexts = [...texts, texto.trim()];
+    // altera o array antigo pelo novo
+    setTexts(newTexts);
+    // salva a nova lista de texts no storage e converte o array newTexts em uma string JSON usando JSON.stringify salvndo na chave texts
+    await AsyncStorage.setItem("texts", JSON.stringify(newTexts));
+    // limpa o input e atualiza a variavel useState texto
+    setTexto("");
+  }
+
+  async function delTexts() {
+    // limpa tds os valores para chave texts da storage de sessão
+    await AsyncStorage.removeItem("texts");
+    // limpa a variavel useState text
+    setTexts([]);
+  }
   return (
     <View style={styles.container}>
       <TextInput style={styles.input} onChangeText={setTexto} value={texto} />
       <View>
-        <Button title="Salvar" onPress={salvarChaveValor} />
-        <Button title="Apagar" onPress={apagarChaves} />
+        <Button title="Save" onPress={saveText} />
+        <Button title="Delete" onPress={delTexts} />
       </View>
       <FlatList
-        data={storageData}
-        keyExtractor={(item) => item.key}
-        renderItem={({ item }) => (
-          <Text style={styles.item}>
-            {item.key}: {item.value}
-          </Text>
-        )}
+        data={texts}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text style={styles.txt}>{item}</Text>}
       />
     </View>
   );
@@ -77,7 +67,7 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: "#f5f5f5",
   },
-  item: {
+  txt: {
     fontSize: 16,
     marginVertical: 4,
     padding: 12,
